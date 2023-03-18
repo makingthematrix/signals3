966b4e2feb0b8856759a8097d6bcbf4146d6c021
Maciej Gorywoda <maciej.gorywoda@jetbrains.com>
2023-03-18 14:03:27 +0100

    generate - an utility method for simplifying creation of more complicated generators

--- src/test/scala/io/github/makingthematrix/signals3/GeneratorsSpec.scala
+++ src/test/scala/io/github/makingthematrix/signals3/GeneratorsSpec.scala
@@ -1,7 +1,7 @@
 package io.github.makingthematrix.signals3
 
 import testutils.{awaitAllTasks, waitForResult}
-
+import scala.collection.mutable
 import scala.concurrent.duration.*
 
 class GeneratorsSpec extends munit.FunSuite:
@@ -20,4 +20,21 @@ class GeneratorsSpec extends munit.FunSuite:
     stream.stop()
     awaitAllTasks
     assert(stream.isStopped)
+  }
+
+  test("test fibonacci event stream") {
+    var a = 0
+    var b = 1
+    val builder = mutable.ArrayBuilder.make[Int]
+    val stream = GeneratorEventStream.generate(100.millis) {
+      val t = a + b
+      a = b
+      b = t
+      t
+    }
+    stream.foreach { builder.addOne }
+    waitForResult(stream, 8)
+    stream.stop()
+    awaitAllTasks
+    assertEquals(builder.result().toSeq, Seq(1, 2, 3, 5, 8))
   }
