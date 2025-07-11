96d6e64ddcc3e915acb2f4a70ae13b7c26a757c4
makingthematrix <makingthematrix@protonmail.com>
2025-07-11 17:46:13 +0200

    implementing a virtual dispatch queue

--- src/main/scala/io/github/makingthematrix/signals3/Threading.scala
+++ src/main/scala/io/github/makingthematrix/signals3/Threading.scala
@@ -1,7 +1,9 @@
 package io.github.makingthematrix.signals3
 
-import DispatchQueue.Unlimited
+import DispatchQueue.{Virtual, Unlimited}
+
 import scala.concurrent.ExecutionContext
+import scala.util.Try
 
 /** Use `Threading` to set up the default execution context which will be later used as the parent for other
   * dispatch queues and to run closeable futures, event streams, and signals, if no other execution context
@@ -20,7 +22,8 @@ object Threading:
   final val Cpus: Int = math.max(2, Runtime.getRuntime.availableProcessors)
 
   private var instance = Option.empty[DispatchQueue]
-  private lazy val defaultQueue = DispatchQueue(Unlimited, ExecutionContext.global)
+  private lazy val defaultQueue = 
+    DispatchQueue(if virtualThreadsSupported() then Virtual else Unlimited, ExecutionContext.global)
 
   /** The default dispatch queue for Wire Signals is lazily initialized, meaning that at the start of the app,
     * before it is used for the first time, you can provide a dispatch queue of your own to act as the default one.
@@ -43,3 +46,13 @@ object Threading:
     *         created at the moment of first use.
     */
   def apply(): DispatchQueue = instance.getOrElse(defaultQueue)
+
+  /**
+   * Checks if virtual threads are supported in the current Java runtime.
+   * Virtual threads were introduced in Java 21.
+   *
+   * @return true if virtual threads are supported, false otherwise
+   */
+  private def virtualThreadsSupported(): Boolean = 
+    Try(classOf[Thread].getMethod("ofVirtual")).isSuccess // tries to access the Thread.ofVirtual() method which exists only in Java 21+
+  
