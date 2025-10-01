3ce57bbd356ea93f2b95105e500be284a7a43059
makingthematrix <makingthematrix@protonmail.com>
2025-10-01 20:36:25 +0200

    refactoring CanBeClosed and Closeable

--- src/main/scala/io/github/makingthematrix/signals3/FlagSignal.scala
+++ src/main/scala/io/github/makingthematrix/signals3/FlagSignal.scala
@@ -3,11 +3,8 @@ package io.github.makingthematrix.signals3
 import scala.concurrent.ExecutionContext
 
 final class FlagSignal extends Signal(Some(false)) {
-  inline def doneIf(p: Boolean)(using ec: ExecutionContext = Threading.defaultContext): Unit =
-    if (p) done()
-  def done()(using ec: ExecutionContext = Threading.defaultContext): Unit = 
-    set(Some(true), Some(ec))
+  inline def doneIf(p: Boolean)(using ec: ExecutionContext = Threading.defaultContext): Unit = if (p) done()
+  def done()(using ec: ExecutionContext = Threading.defaultContext): Unit = set(Some(true), Some(ec))
   inline def state: Boolean = value.contains(true)
-  def onDone(body : => Unit)(using ec: ExecutionContext = Threading.defaultContext): Unit = 
-    this.foreach(_ => body)
+  def onDone(body : => Unit)(using ec: ExecutionContext = Threading.defaultContext): Unit = this.foreach(_ => body)
 }
