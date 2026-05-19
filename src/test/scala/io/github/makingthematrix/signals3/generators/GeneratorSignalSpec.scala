2f37f8cb5a5ea3e06c249c808d19c32d8e9b315e
makingthematrix <makingthematrix@protonmail.com>
2026-05-19 09:27:01 +0200

    speeding up unit tests

--- src/test/scala/io/github/makingthematrix/signals3/generators/GeneratorSignalSpec.scala
+++ src/test/scala/io/github/makingthematrix/signals3/generators/GeneratorSignalSpec.scala
@@ -1,32 +1,38 @@
 package io.github.makingthematrix.signals3.generators
 
-import io.github.makingthematrix.signals3.testutils.{awaitAllTasks, waitForResult}
-import io.github.makingthematrix.signals3.{DoneSignal, EventContext, FlagSignal, Signal, Threading}
+import io.github.makingthematrix.signals3.testutils.{awaitAllTasks, waitFor}
+import io.github.makingthematrix.signals3.{DispatchQueue, DoneSignal, EventContext, FlagSignal, SerialDispatchQueue, Signal}
 
 import java.util.concurrent.TimeUnit
 import scala.collection.mutable
 import scala.concurrent.duration.*
 
 class GeneratorSignalSpec extends munit.FunSuite {
-  import EventContext.Implicits.global
-  import Threading.defaultContext
+  private val eventContext = EventContext()
+  given dq: DispatchQueue = SerialDispatchQueue()
 
-  def fibDelay(t: (Int, Int)): FiniteDuration = FiniteDuration(t._2 * 200L, TimeUnit.MILLISECONDS)
+  override def beforeEach(context: BeforeEach): Unit =
+    eventContext.start()
 
-  given Timeout: FiniteDuration = 5.seconds
+  override def afterEach(context: AfterEach): Unit =
+    eventContext.stop()
+
+  given Timeout: FiniteDuration = 3.seconds
+  val HeartBeatMs: FiniteDuration = 100.millis
+
+  def fibDelay(t: (Int, Int)): FiniteDuration = FiniteDuration(t._2 * HeartBeatMs.toMillis, TimeUnit.MILLISECONDS)
 
   test("fibonacci signal with generate") {
     val builder = mutable.ArrayBuilder.make[Int]
     val isSuccess = DoneSignal()
   
-    val signal = GeneratorSignal.generate((0, 1), 200.millis) { case (a, b) => (b, a + b) }
+    val signal = GeneratorSignal.generate((0, 1), HeartBeatMs) { case (a, b) => (b, a + b) }
     signal.foreach { case (_, b) =>
       builder.addOne(b)
       if (b == 8) isSuccess.done()
     }
-    waitForResult(isSuccess, true, Timeout)
+    waitFor(isSuccess, true)
     signal.close()
-    awaitAllTasks
     assertEquals(builder.result().toSeq, Seq(1, 1, 2, 3, 5, 8))
   }
 
@@ -40,9 +46,8 @@ class GeneratorSignalSpec extends munit.FunSuite {
       builder.addOne(b)
       isSuccess.doneIf(b == 8)
     }
-    waitForResult(isSuccess, true, Timeout)
+    waitFor(isSuccess, true)
     signal.close()
-    awaitAllTasks
     assertEquals(builder.result().toSeq, Seq(1, 2, 3, 5, 8))
     assert(signal.isClosed)
   }
@@ -58,14 +63,13 @@ class GeneratorSignalSpec extends munit.FunSuite {
       isSuccess ! (b == 5)
     }
   
-    waitForResult(isSuccess, true, Timeout)
+    waitFor(isSuccess, true)
 
     val totalTime = System.currentTimeMillis - now
     signal.close()
-    awaitAllTasks
     assertEquals(builder.result().toSeq, Seq(1, 1, 2, 3, 5))
-    // it should take (100 + 100 + 200 + 300)ms * 2 + 100ms for a buffer, but still it can be flaky
-    assert(totalTime >= 1400 && totalTime <= 1500L, s"total time: $totalTime")
+    // it should take (100 + 100 + 200 + 300)ms + 100ms for a buffer, but still it can be flaky
+    assert(totalTime >= 700 && totalTime <= 1000L, s"total time: $totalTime")
   }
 
   test("fibonacci signal with variant unfold with delays also in fibonacci") {
@@ -79,32 +83,31 @@ class GeneratorSignalSpec extends munit.FunSuite {
       isSuccess ! (b == 5)
     }
 
-    waitForResult(isSuccess, true, Timeout)
+    waitFor(isSuccess, true)
 
     val totalTime = System.currentTimeMillis - now
     signal.close()
-    awaitAllTasks
     assertEquals(builder.result().toSeq, Seq(1, 2, 3, 5))
-    // it should take (100 + 200 + 300 + 500)ms * 2 + 100ms for a buffer, but still it can be flaky
-    assert(totalTime >= 2200 && totalTime <= 2500L, s"total time: $totalTime")
+    // it should take (100 + 200 + 300 + 500)ms + 100ms for a buffer, but still it can be flaky
+    assert(totalTime >= 1100 && totalTime <= 1500L, s"total time: $totalTime")
     assert(signal.isClosed)
   }
 
   test("add pause to counter generator signal") {
     val isSuccess = Signal(false)
-    val signal1 = GeneratorSignal.counter(200.millis)
+    val signal1 = GeneratorSignal.counter(HeartBeatMs)
     signal1.foreach { counter =>
       isSuccess ! (counter == 4)
     }
     val t1 = System.currentTimeMillis
-    waitForResult(isSuccess, true, Timeout)
+    waitFor(isSuccess, true)
     val timePassed1 = System.currentTimeMillis - t1
     signal1.close()
     awaitAllTasks
 
     // restart, this time with pause for one beat when counter == 2
     isSuccess ! false
-    waitForResult(isSuccess, false)
+    waitFor(isSuccess, false)
 
     var pausedOn = 0L
 
@@ -113,35 +116,36 @@ class GeneratorSignalSpec extends munit.FunSuite {
         pausedOn = System.currentTimeMillis
         true
       } else {
-        System.currentTimeMillis - pausedOn < 300L
+        System.currentTimeMillis - pausedOn < 150L
       }
-    val signal2 = GeneratorSignal(0, (n: Int) => n + 1, 200.millis, paused)
+
+    val signal2 = GeneratorSignal(0, (n: Int) => n + 1, HeartBeatMs, paused)
 
     signal2.foreach { counter =>
       isSuccess ! (counter == 4)
     }
 
     val t2 = System.currentTimeMillis
-    waitForResult(isSuccess, true, Timeout)
+    waitFor(isSuccess, true)
     val timePassed2 = System.currentTimeMillis - t2
     signal2.close()
     awaitAllTasks
-    assert(timePassed2 - timePassed1 >= 200, timePassed2)
+    assert(timePassed2 - timePassed1 >= HeartBeatMs.toMillis, timePassed2)
     assert(pausedOn > 0L)
   }
 
   test("Close the generator signal automatically") {
     import scala.util.Using
     val arr = mutable.ArrayBuilder.make[Int]
-    Using(GeneratorSignal.counter(200.millis)) { sig =>
+    Using(GeneratorSignal.counter(HeartBeatMs)) { sig =>
       sig.foreach(n => arr.addOne(n))
-      Thread.sleep(500L)
+      Thread.sleep(HeartBeatMs.toMillis * 2L)
     }
     val res1 = arr.result().toSeq
     assert(res1.nonEmpty)
     // the generator itself is not available here anymore (as it should be)
     // so to check if it's closed we wait a little and see if a new value was generated (it shouldn't be)
-    Thread.sleep(500L)
+    Thread.sleep(HeartBeatMs.toMillis * 2L)
     val res2 = arr.result().toSeq
     assertEquals(res1, res2)
   }
@@ -149,15 +153,15 @@ class GeneratorSignalSpec extends munit.FunSuite {
   test("Close the generator stream automatically") {
     import scala.util.Using
     val arr = mutable.ArrayBuilder.make[Int]
-    Using(GeneratorStream.repeat(1, 200.millis)) { stream =>
+    Using(GeneratorStream.repeat(1, HeartBeatMs)) { stream =>
       stream.foreach(n => arr.addOne(n))
-      Thread.sleep(500L)
+      Thread.sleep(HeartBeatMs.toMillis * 2L)
     }
     val res1 = arr.result().toSeq
     assert(res1.nonEmpty)
     // the generator itself is not available here anymore (as it should be)
     // so to check if it's closed we wait a little and see if a new value was generated (it shouldn't be)
-    Thread.sleep(500L)
+    Thread.sleep(HeartBeatMs.toMillis * 2L)
     val res2 = arr.result().toSeq
     assertEquals(res1, res2)
   }
@@ -172,9 +176,8 @@ class GeneratorSignalSpec extends munit.FunSuite {
       done.setIf(n >= 4)
     }
 
-    waitForResult(done, true)
+    waitFor(done, true)
     signal.close()
-    awaitAllTasks
     assertEquals(last, 4)
   }
 
@@ -182,15 +185,14 @@ class GeneratorSignalSpec extends munit.FunSuite {
     val values = Seq(10, 20, 30, 40)
     val buf = mutable.ArrayBuffer[Int]()
     val done = FlagSignal()
-    val sig = GeneratorSignal.from(values, 50.millis)
+    val sig = GeneratorSignal.from(values, (HeartBeatMs.toMillis / 4L).millis)
 
     sig.foreach { v =>
       buf.addOne(v)
       done.setIf(v == values.last)
     }
 
-    waitForResult(done, true)
-    awaitAllTasks
+    waitFor(done, true)
     assert(sig.isClosed)
     // FiniteGeneratorSignal starts with the first value; foreach records subsequent changes as they occur.
     // The collected sequence should contain all values, in order, ending with the last one.
@@ -209,8 +211,7 @@ class GeneratorSignalSpec extends munit.FunSuite {
     sig.onClose(finished ! true)
     sig.foreach(buf.addOne)
 
-    waitForResult(finished, true)
-    awaitAllTasks
+    waitFor(finished, true)(using duration = 1.seconds)
     assert(sig.isClosed)
     assertEquals(buf.toSeq, Seq(1, 2, 3, 4, 5))
   }
@@ -222,13 +223,12 @@ class GeneratorSignalSpec extends munit.FunSuite {
     val sig: FiniteGeneratorSignal[Int] = FiniteGeneratorSignal(() => {
       n += 1
       if (n <= 3) Some(n) else None
-    }, 60.millis)
+    }, 40.millis)
 
     sig.onClose(finished ! true)
     sig.foreach(buf.addOne)
 
-    waitForResult(finished, true)
-    awaitAllTasks
+    waitFor(finished, true)(using duration = 1.seconds)
     assert(sig.isClosed)
     assertEquals(buf.toSeq, Seq(1, 2, 3))
   }
@@ -244,7 +244,7 @@ class GeneratorSignalSpec extends munit.FunSuite {
       done.setIf(v == 5)
     }
 
-    waitForResult(done, true)
+    waitFor(done, true)
     assertEquals(buf.toSeq, Seq(1, 2, 3, 4, 5))
   }
 
@@ -259,12 +259,11 @@ class GeneratorSignalSpec extends munit.FunSuite {
       finishedInit.setIf(v == values.init.last)
     }
 
-    waitForResult(finishedInit, true)
+    waitFor(finishedInit, true)(using duration = 1.seconds)
     // Also ensure the underlying generator completes
     val allDone = Signal(false)
     sig.onClose(allDone ! true)
-    waitForResult(allDone, true)
-    awaitAllTasks
+    waitFor(allDone, true)(using duration = 1.seconds)
 
     assertEquals(bufInit.toSeq, values.init)
     assert(sig.isClosed)
@@ -272,7 +271,7 @@ class GeneratorSignalSpec extends munit.FunSuite {
 
   test("CloseableGeneratorSignal.onClose is invoked on close") {
     var closed = false
-    val sig = GeneratorSignal(0, (n: Int) => n + 1, 80.millis)
+    val sig = GeneratorSignal(0, (n: Int) => n + 1, 40.millis)
     sig.onClose { closed = true }
     val done = FlagSignal()
     var cnt = 0
@@ -281,15 +280,14 @@ class GeneratorSignalSpec extends munit.FunSuite {
       done.setIf(cnt == 2)
     }
 
-    waitForResult(done, true)
+    waitFor(done, true)
     sig.close()
-    awaitAllTasks
     assert(sig.isClosed)
     assert(closed)
   }
 
   test("closing the generator signal twice returns false") {
-    val sig = GeneratorSignal.counter(100.millis)
+    val sig = GeneratorSignal.counter(HeartBeatMs)
     assert(sig.closeAndCheck())
     assert(sig.isClosed)
     assert(!sig.closeAndCheck())
